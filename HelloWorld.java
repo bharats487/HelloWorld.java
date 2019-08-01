28b4bce44e27b47845f1e1c7f68890bf45fd3b94
bharats487 <43616706+bharats487@users.noreply.github.com>
2019-08-01 01:19:49 -0500

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,9 +1,6 @@
-public class Main {
-
-    public static void main(String[] args) {
-
-        System.out.println("Goodbye, World!");
-
-    }
-
+public class HelloWorld {
+   public static void main(String[] args) {
+      // Prints "Hello, World" in the terminal window.
+      System.out.println("Hello, World");
+   }
 }
